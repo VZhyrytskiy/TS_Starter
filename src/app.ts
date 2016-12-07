4f2677af83b463c21cba654656c7872bea65640f
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2016-12-07 18:10:42 +0200

    t-01, t-02

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,60 @@
-showHello("greeting", "TypeScript");
 
-function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
-    elt.innerText = `Hello from ${name}`;
+enum Category { JavaScript, CSS, HTML, TypeScript, Angular2 }
+
+function getAllBooks() {
+    
+    let books =[
+        { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JavaScript },
+        { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JavaScript },
+        { title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
+        { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JavaScript }
+    ];
+    
+    return books;
 }
 
+function logFirstAvailable(books): void {
+    
+    let numberOfBooks: number = books.length;
+    let firstAvailable: string = '';
+
+    for(let currentBook of books) {
+        if(currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    
+    console.log(`Total Books: ${numberOfBooks}`);
+    console.log(`First Available: ${firstAvailable}`);
+}
+
+function getBookTitlesByCategory(categoryFilter: Category): Array<string> {
+    console.log(`Getting books in category: ${Category[categoryFilter]}`);
+    
+    const allBooks = getAllBooks();
+    const filteredTitles: string[] = [];
+    
+    for(let currentBook of allBooks) {
+        if(currentBook.category === categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+    
+    return filteredTitles;
+}
+
+function logBookTitles(titles: string[]): void {
+    for(let title of titles) {
+        console.log(title);
+    }
+}
+
+// ---------------------------------------------
+console.log(getAllBooks());
+
+const allBooks = getAllBooks();
+logFirstAvailable(allBooks);
+
+const javaScriptBooks = getBookTitlesByCategory(Category.JavaScript);
+logBookTitles(javaScriptBooks);
