f3d23be6a6dbc72d782747d59131a5bcf3355b2d
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2016-12-28 10:33:51 +0200

    namespace app

---
+++ src/NameSpaceDemo/app.js
@@ -0,0 +1,4 @@
+/// <reference path="utility-functions.ts" />
+var util = Utility.Fees;
+var fee = util.CalculateLateFee(10);
+console.log("Fee: " + fee);
---
+++ src/NameSpaceDemo/app.ts
@@ -0,0 +1,6 @@
+/// <reference path="utility-functions.ts" />
+
+import util = Utility.Fees;
+
+let fee = util.CalculateLateFee(10);
+console.log(`Fee: ${fee}`);
