bca5c3fcba844569d0bece1756128f2da78a6c3f
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-07-24 15:04:31 +0300

    fix20.2

--- src/decorators.ts
+++ src/decorators.ts
@@ -12,8 +12,17 @@ export function logger<TFunction extends Function>(
   let newConstructor: Function = function() {
     console.log(`Creating new instance.`);
     console.log(target);
+
+    this.age = 30;
   };
+  // это нужно так, как добавлен декоратор sealed и он запрещает обавлять методы
   newConstructor.prototype = Object.create(target.prototype);
   newConstructor.prototype.constructor = target;
+
+  // добавим новый метод
+  newConstructor.prototype.printLibrarian = function() {
+    console.log(`Librarian name:  ${this.name}, Librarian age: ${this.age}`);
+  };
+
   return <TFunction>newConstructor;
 }
