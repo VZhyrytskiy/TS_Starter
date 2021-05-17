e3e74bc8edbb395ad7915dc5074d7daa85b8525a
upskill js.basics <upskill.js.basics@gmail.com>
2021-05-17 17:07:24 +0300

    feat: add task about conditional types

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,13 @@ export type BookRequiredFields = Required<Book>;
 export type UpdatedBook = Partial<Book>;
 export type AuthorWoEmail = Omit<Author, 'email'>;
 export type createCustomerFunctionType = (name: string, age?: number, city?: string) => void;
+
+type fn = (a: string, b: number, c: boolean) => symbol;
+type Param1<T> = T extends (a: infer R, b: number, c: boolean) => symbol ? R : never;
+type Param2<T> = T extends (a: string, b: infer R, c: boolean) => symbol ? R : never;
+type Param3<T> = T extends (a: string, b: number, c: infer R) => symbol ? R : never;
+export type P1 = Param1<fn>; 
+export type P2 = Param2<fn>; 
+export type P3 = Param3<fn>; 
+
+
